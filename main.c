95d3ad5a13862f96e64ddae6f45896e5ac4f8b85
Adam Tecle <adam.tecle@gmail.com>
2013-11-24 23:28:15 -0500

    thread array

--- main.c
+++ main.c
@@ -26,17 +26,29 @@ int main(int argc, char **argv) {
     }
   
     create_cat(argv[3]);
-    //Customer *customers = NULL;
     create_db(db);
+    read_orders(orders); //main thread of execution acting as producer thread
     
-    //if (customers == NULL) printf("NULL\n");
+
+    
+    pthread_t thread_array[100];
+    int id[100];
+    
+    int i = 0;
+    
+    for (cat; cat != NULL; cat = cat->hh.next) {
+        int ord_index = cat->index;
+        id[i] = pthread_create(&thread_array[i], NULL, (void*)&process_order, (void*)bookorders[ord_index]);
+        i++;
+    }
+    
+    int j;
+    for (j = 0; j <= i; j++) {
+        pthread_join(thread_array[j], NULL);
+    }
+    
     
     pthread_t thr1, thr2;
-    
-    read_orders(orders);
- 
-    //pthread_create(&thr1, NULL, (void*)&read_orders, (void*)orders);
-    //pthread_join(thr1, NULL);
     
     return 1;
     
